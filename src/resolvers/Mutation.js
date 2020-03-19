a18152b8cf920bcce99c9a618ac923e8a06ca1cd
Larissa <larissasilvavarjao@gmail.com>
2020-03-19 20:13:34 -0300

    feat(createItem): add first mutation to schema

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,9 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
 
-module.exports = mutations;
+    return item;
+  }
+};
+
+module.exports = Mutations;
