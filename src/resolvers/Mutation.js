3e48feaecb7825d0f9aee35306fa256f1edcd905
Larissa <larissasilvavarjao@gmail.com>
2020-03-20 14:51:39 -0300

    feat(item): query and mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,6 +3,20 @@ const Mutations = {
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
 
     return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+
+    return await ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
