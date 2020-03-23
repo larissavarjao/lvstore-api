f8dbd5b5670082fe35adf94ffd2aac6ff159d725
Larissa <larissasilvavarjao@gmail.com>
2020-03-23 17:59:29 -0300

    feat(user): me query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,7 +3,15 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
-  itemsConnection: forwardTo('db')
+  itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    const id = ctx.request.userId;
+    if (!id) {
+      return null;
+    }
+
+    return ctx.db.query.user({ where: { id } }, info);
+  }
 };
 
 module.exports = Query;
