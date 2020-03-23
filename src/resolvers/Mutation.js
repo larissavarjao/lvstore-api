ea332133ab132e15d50a1b6e2591235ed0d9bd8b
Larissa <larissasilvavarjao@gmail.com>
2020-03-23 20:23:54 -0300

    feat(user): signout mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -68,6 +68,11 @@ const Mutations = {
     });
 
     return user;
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+
+    return { message: 'Goodbye' };
   }
 };
 
