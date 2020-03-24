44032d5a8d868f293ebf69adb3e1c855d94c222a
Larissa <larissasilvavarjao@gmail.com>
2020-03-23 21:33:13 -0300

    feat(user): reset password mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,12 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
+
+const cookieVariables = {
+  httpOnly: true,
+  maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+};
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -43,10 +50,7 @@ const Mutations = {
 
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
 
-    ctx.response.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
-    });
+    ctx.response.cookie('token', token, cookieVariables);
 
     return user;
   },
@@ -62,10 +66,7 @@ const Mutations = {
     }
 
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-    ctx.response.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
-    });
+    ctx.response.cookie('token', token, cookieVariables);
 
     return user;
   },
@@ -73,6 +74,50 @@ const Mutations = {
     ctx.response.clearCookie('token');
 
     return { message: 'Goodbye' };
+  },
+  async requestReset(parent, args, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+
+    if (!user) {
+      throw new Error('There is no email/password with this values');
+    }
+
+    const randomBytesPromisified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromisified(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000; // one hour from now
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry }
+    });
+
+    console.log(res);
+    return { message: 'Hope you figure out your new password!' };
+  },
+  async resetPassword(parent, args, ctx, info) {
+    if (args.password !== args.confirmPassword) {
+      throw new Error("Your password don't match!");
+    }
+
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000
+      }
+    });
+
+    if (!user) {
+      throw new Error('Token expired!');
+    }
+
+    const password = await bcrypt.hash(args.password, 10);
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: { password, resetToken: null, resetTokenExpiry: null }
+    });
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, cookieVariables);
+
+    return updatedUser;
   }
 };
 
